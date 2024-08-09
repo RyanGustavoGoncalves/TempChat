ee35f62a7c2a6d16c8fd34b9935a29824cfcadb1
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-08-09 20:44:22 -0300

    feat: Add Home and ErrorNotFoundPage components
    This commit adds the Home and ErrorNotFoundPage components to the frontend application. The Home component renders a simple "Home" heading, while the ErrorNotFoundPage component displays a "404" heading and a "Page not found" message. These components enhance the user interface of the application and improve the overall user experience.

---
+++ frontend/src/global/assets/utils/config/infra/ProtectedRoute.tsx
@@ -0,0 +1,64 @@
+import { url } from '@/infra/url';
+import { useState, useEffect, ComponentType } from 'react';
+import { Navigate } from 'react-router-dom';
+import { toast } from 'sonner';
+
+interface ProtectedRouteProps {
+  element: ComponentType<any>;
+  [key: string]: any;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element: Element, ...rest }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const token = localStorage.getItem('token');
+
+  useEffect(() => {
+    const checkToken = async () => {
+      if (token) {
+        try {
+          const response = await fetch(`${url}/token`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          if (response.ok) {
+            const responseBody = await response.json();
+            const role = responseBody.role;
+            localStorage.setItem('role', role);
+            setIsAuthenticated(true);
+          } else {
+            localStorage.removeItem('role');
+            setIsAuthenticated(false);
+
+            // Exibir alerta de erro
+            toast.error("Error", {
+              description: "Invalid or expired token.",
+            });
+          }
+        } catch (error) {
+          // Exibir alerta de erro
+          toast.error("Error", {
+            description: "Error fetching token!",
+          });
+
+          localStorage.removeItem('role');
+          setIsAuthenticated(false);
+        }
+      } else {
+        localStorage.removeItem('role');
+        setIsAuthenticated(false);
+      }
+    };
+
+    checkToken();
+  }, [token, setIsAuthenticated]);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? <Element {...rest} /> : <Navigate to="/Welcome" />;
+};
+
+export default ProtectedRoute;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './global/assets/css/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/home/Home.tsx'
+import ErrorNotFoundPage from './pages/error/notFoundPage/ErrorNotFoundPage.tsx'
+import ProtectedRoute from './global/assets/utils/config/infra/ProtectedRoute.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoute element={App} />,
+    errorElement: <ErrorNotFoundPage />,
+    children: [
+      {
+        path: "Home",
+        element: <Home />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
