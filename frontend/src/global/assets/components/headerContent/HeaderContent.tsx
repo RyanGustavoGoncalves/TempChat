cedc788be1ce6890fb73c5c92bafbd460db1bb83
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-10-02 22:14:19 -0300

    Refactor package.json to include @radix-ui/react-separator

--- frontend/src/global/assets/components/headerContent/HeaderContent.tsx
+++ frontend/src/global/assets/components/headerContent/HeaderContent.tsx
@@ -1,16 +1,14 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Input } from "@/components/ui/input";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
-import { Home, MessageCircle, PanelLeft, Search } from "lucide-react";
+import { MessageCircle, PanelLeft } from "lucide-react";
 import UserProfile from "../userProfile/UserProfile";
 import { ModeToggle } from "@/components/ModeToggle";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 const HeaderContent = () => {
     return (
-        <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+        <header className="sticky top-0 z-100 p-6 flex h-screen gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <Sheet>
                 <SheetTrigger asChild>
                     <Button size="icon" variant="outline" className="sm:hidden">
@@ -54,33 +52,6 @@ const HeaderContent = () => {
                     </nav>
                 </SheetContent>
             </Sheet>
-            <Breadcrumb className="hidden md:flex">
-                <BreadcrumbList>
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link to="#">Dashboard</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbLink asChild>
-                            <Link to="#">Products</Link>
-                        </BreadcrumbLink>
-                    </BreadcrumbItem>
-                    <BreadcrumbSeparator />
-                    <BreadcrumbItem>
-                        <BreadcrumbPage>All Products</BreadcrumbPage>
-                    </BreadcrumbItem>
-                </BreadcrumbList>
-            </Breadcrumb>
-            <div className="relative ml-auto flex-1 md:grow-0">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                    type="search"
-                    placeholder="Search..."
-                    className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
-                />
-            </div>
         </header>
     )
 
--- frontend/src/global/assets/components/navbar/Navbar.tsx
+++ frontend/src/global/assets/components/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import UserProfile from "../userProfile/UserProfile";
 
 const Navbar = () => {
     return (
-        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-secondary sm:flex">
             <nav className="flex flex-col items-center justify-between gap-4 px-2 py-4 h-full">
                 <Link
                     to="#"
